14fe80f99d73da887fe0c8338a753eade7440dce
k2491p <fukuda.dev10@gmail.com>
2022-03-27 17:09:38 +0900

    add BadExample

---
+++ DIP/DIP/Bad/Video.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIP.Bad
+{
+public sealed class Video
+{
+    public void Record()
+    {
+        // ビデオに録画
+    }
+}
+}
--- DIP/DIP/Television.cs
+++ DIP/DIP/Television.cs
@@ -7,8 +7,35 @@ namespace DIP
     {
         static void Main(string[] args)
         {
+            RecordGood();
+
+            RecordBad()
+        }
+
+        public static void RecordGood()
+        {
             IRecord recorder = Factories.CreateRecord();
             recorder.Record();
+        }
+
+        public static void RecordBad()
+        {
+            if (ConnectsVideo())
+            {
+                Video video = new Video();
+                video.Record();
+                return;
+            }
+
+            if (ConnectsBluRay())
+            {
+                Blu_ray bluRay = new Blu_ray();
+                bluRay.Record();
+                return;
+            }
+
+            HDD hdd = new HDD();
+            hdd.Record();
         }
     }
 }
